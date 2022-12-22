9312615162c484f597b5fe487ab8fb7f35c274c9
Nora Khalil <khalil.nor@northeastern.edu>
2022-12-22 16:13:20 -0500

    first commit

---
+++ 2_BTP_optimization/flame_speeds/NIST_with_and_without_M/flame_speed_run_range_10pts_no_M.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#SBATCH --nodes=1
+#SBATCH --time=4-00:00:00
+#SBATCH --job-name=without_halocarbene_recombination
+#SBATCH --error=error.slurm.log
+#SBATCH --output=output.slurm.log
+##SBATCH --cpus-per-task=5
+##SBATCH --mem-per-cpu=8Gb
+##SBATCH --ntasks=1 
+##SBATCH --array=1
+#SBATCH --partition=west
+
+
+source activate cantera_env
+python flame_speed_calc_range_10pts_no_M.py
